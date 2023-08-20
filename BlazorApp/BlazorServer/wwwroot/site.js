d6c839b129c1bd76a433e84ee81e09f783659314
Zack Zhang <realzhezhang@gmail.com>
2023-08-20 14:39:59 -0700

    Initial Commit

---
+++ BlazorApp/BlazorServer/wwwroot/site.js
@@ -0,0 +1,15 @@
+﻿function scrollToSection(sectionId) {
+    let target;
+
+    if (sectionId === 'home') {
+        // Scroll to the top of the page
+        target = document.body;
+    } else {
+        // Scroll to the specific section
+        target = document.getElementById(sectionId);
+    }
+
+    if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+    }
+}
